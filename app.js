92ef69a0b411f2c30464567a00c93736394b6452
Junaid Farooq <junaid@evercam.io>
2016-03-26 15:38:45 +0500

    Removed bower technique

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", function(req,res){
   res.send("hello world")
 });
 
+app
+  .set("view engine", "ejs")
+  .use(express.static("lib"));
+
 app.listen(3000,function(){
   console.log("App is listing at port");
 });
